e31eccb5fe1d73c58eb23e67b1a1a44fea66b36d
luka <luka.ivankovic2@fer.hr>
2021-12-16 18:54:45 +0100

    dodane opcije logout i user u test.js

--- IzvorniKod/frontend/src/Test.js
+++ IzvorniKod/frontend/src/Test.js
@@ -3,28 +3,33 @@ import './styles/App.css';
 
 function Test(props){
 
-    function onClick(event){
+    function onClickUser(event){
         event.preventDefault();
-        const body =  `username=admin&password=pass`;
 
         const options = {
             method: 'GET',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            //body: body,
             mode: "no-cors"
         };
 
         fetch('/user', options).then(response => response.json()).then(response => console.log(response));
     }
 
+    function onClickLogout(event){
+        event.preventDefault();
+
+        const options = {
+            mode: "no-cors"
+        };
+
+        fetch('/logout', options).then(response => console.log(response));
+    }
+
     return(
         <div className="Test">
             <h2>Test</h2>
-            <div>
-            <button onClick={onClick}>Logout</button>
-            </div>
+            <div><button onClick={onClickUser}>User</button></div>
+            <div><button onClick={onClickLogout}>Logout</button></div>
+            
             
         </div>
     )
