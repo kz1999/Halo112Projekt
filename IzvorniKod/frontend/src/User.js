55960804d3c02d1d1c9b4a460021b2a316370c03
luka <luka.ivankovic2@fer.hr>
2021-12-17 01:12:55 +0100

    polish upanje i dodan gumb za confirmanje (ne i funkcionalnost gumba)

--- IzvorniKod/frontend/src/User.js
+++ IzvorniKod/frontend/src/User.js
@@ -6,21 +6,20 @@ function User(props){
 
     function onClick(event){
         event.preventDefault();
-        props.setUserToChange(props.user.userName);
+        //promjeni iz unconfirmed u confirmed (post na /korisnici/username)
     }
 
     return(
         <div className="flex-container">
-            <div className="link" onClick={onClick}>{props.user.userName}</div>
+            <div className="link" onClick={props.setUserToChange(props.user.userName)}>{props.user.userName}</div>
             <div>{props.user.password}</div>
             <div>{props.user.email}</div>
             <div>{props.user.name}</div>
             <div>{props.user.lastName}</div>
             <div>{props.user.phoneNumber}</div>
             <div>{props.user.role}</div>
-            <div>{props.user.confirmed.toString()}</div>
+            <div onClick={onClick}>{props.user.confirmed.toString()}</div>
         </div>
-        
     )
 }
 
