0bfd0715ff6a793b1dc48ca06b7ef0aade9ca51b
bankoleon <leon.banko@fer.hr>
2021-12-17 20:12:34 +0100

    popravljen login

---
+++ IzvorniKod/backend/src/main/java/com/example/halo112_generic/rest/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,21 @@
+package com.example.halo112_generic.rest;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+  @Override
+  public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+      Authentication authentication) throws IOException, ServletException {
+    clearAuthenticationAttributes(request);
+    response.setStatus(HttpStatus.ACCEPTED.value());
+  }
+}
