eb142d8643c986c7dbee4b28659f90f9835129d4
Krunoslav Zadrić <kz51900@fer.hr>
2021-12-18 12:46:35 +0100

    ispravljen prikaz mape

--- IzvorniKod/frontend/src/App.js
+++ IzvorniKod/frontend/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 import './styles/App.css';
+import 'leaflet/dist/leaflet.css';
 
 import Login from './Login';
 import Register from './Register';
 import {HeaderLoggedIn, HeaderLoggedOut} from './Header';
 import Users from './Users';
 import Test from './Test';
-import {MapContainer, TileLayer} from 'react-leaflet';
+import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
 
 function App() {
 
@@ -57,9 +58,17 @@ function App() {
       </div>
     </BrowserRouter>
 
-    <MapContainer center={[40.505, -100.09]} zoom={13} >
-      <TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
-    </MapContainer>
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+  <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+  <Marker position={[51.505, -0.09]}>
+    <Popup>
+      A pretty CSS3 popup. <br /> Easily customizable.
+    </Popup>
+  </Marker>
+</MapContainer>
     </React.Fragment>
   );
 }
