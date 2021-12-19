2e0d295ad5105ec445b5be75687dad607760e432
bankoleon <leon.banko@fer.hr>
2021-12-19 21:11:44 +0100

    login, registracija, confirm

--- IzvorniKod/backend/src/main/java/com/example/halo112_generic/rest/LoginController.java
+++ IzvorniKod/backend/src/main/java/com/example/halo112_generic/rest/LoginController.java
@@ -1,15 +1,56 @@
 package com.example.halo112_generic.rest;
 
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.halo112_generic.dao.UserRepository;
+import com.example.halo112_generic.domain.User;
 
 @RestController
 @RequestMapping("/login")
 public class LoginController {
-
+	
+	@Autowired
+    private UserRepository userRepo;
+	
     @GetMapping
-    public String login(){
-        return "LOGIN PAGE";
+    public ResponseEntity<String> login() {
+    	Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
+        
+        if (!auth.getAuthorities().stream()
+                .anyMatch(r -> r.getAuthority().equals("ROLE_ANONYMOUS"))) {
+        	return new ResponseEntity<>("already logged in as " + username, HttpStatus.FORBIDDEN);
+        } else {
+        	return new ResponseEntity<>("login page", HttpStatus.OK);
+        }
     }
+    /*
+    @PostMapping
+    public ResponseEntity<String> login(HttpServletRequest req, @RequestParam(name = "username") String username, @RequestParam(name = "password") String password) {
+    	UsernamePasswordAuthenticationToken authReq = new UsernamePasswordAuthenticationToken(username, password);
+    	Authentication auth = authManager.authenticate(authReq);
+    
+    	SecurityContext sc = SecurityContextHolder.getContext();
+    	sc.setAuthentication(auth);
+    	HttpSession session = req.getSession(true);
+    	session.setAttribute(SPRING_SECURITY_CONTEXT_KEY, sc);
+    }
+    */
 }
--- IzvorniKod/backend/src/main/java/com/example/halo112_generic/rest/WebSecurity.java
+++ IzvorniKod/backend/src/main/java/com/example/halo112_generic/rest/WebSecurity.java
@@ -11,7 +11,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -22,7 +21,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     @Autowired
     UserDetailsService userDetailsService;
-    private Object BCryptPasswordEncoder;
+    private PasswordEncoder BCryptPasswordEncoder;
 
     @Bean
     public DaoAuthenticationProvider authProvider(){
@@ -35,10 +34,6 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
         auth.userDetailsService(userDetailsService);
-        auth.inMemoryAuthentication()
-                .withUser("admin")
-                .password("pass")
-                .roles("ADMIN");
         auth.authenticationProvider(authProvider());
     }
 
@@ -56,22 +51,15 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                 .antMatchers("/h2-console").permitAll()
                 .antMatchers("/korisnici").permitAll()
                 .antMatchers("/user").permitAll()
+                .antMatchers("/login").permitAll()
                 .antMatchers("/").permitAll()
                 .and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/")
-                .and().formLogin().loginPage("/login");
+                .and().formLogin().loginPage("/login").failureHandler(new CustomAuthenticationFailureHandler()).successHandler(new CustomAuthenticationSuccessHandler());
         http.headers().frameOptions().sameOrigin(); // fixes h2-console problem
         http.csrf().disable();
     }
 
     @Bean
-    PasswordEncoder passwordEncoder(){ return NoOpPasswordEncoder.getInstance();}
-
-    /*
-    @Bean
-    PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
-     */
-
+    PasswordEncoder passwordEncoder(){ return new BCryptPasswordEncoder();}
 }
 
