2e0b1c7c587132b66db56bca01a65ed2571f395f
Krunoslav Zadrić <kz51900@fer.hr>
2021-12-19 19:34:56 +0100

    sredivanje mape

--- IzvorniKod/frontend/src/App.js
+++ IzvorniKod/frontend/src/App.js
@@ -1,43 +1,60 @@
-import React from 'react';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import './styles/App.css';
-import 'leaflet/dist/leaflet.css';
+import "./styles/App.css";
 
-import Login from './Login';
-import Register from './Register';
-import {HeaderLoggedIn, HeaderLoggedOut} from './Header';
-import Users from './Users';
-import Test from './Test';
-import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
+import Login from "./Login";
+import Register from "./Register";
+import { HeaderLoggedIn, HeaderLoggedOut } from "./Header";
+import Users from "./Users";
+import Test from "./Test";
+import Map from "./Map";
+//import { MapContainer } from "react-leaflet";
 
 function App() {
-
   const [isLoggedIn, setIsLoggedIn] = React.useState(false);
-  const [user, setUser] = React.useState( {username:'', password:'', name:'', lastName:'', phoneNumber:'', email:'', role:'', confirmed:false});
-  
-  function checkUserStatus(){
-    fetch('/user')
-      .then(data => data.json())
-      .then(data =>{
-        if(data === null){
+  const [user, setUser] = React.useState({
+    username: "",
+    password: "",
+    name: "",
+    lastName: "",
+    phoneNumber: "",
+    email: "",
+    role: "",
+    confirmed: false,
+  });
+
+  function checkUserStatus() {
+    fetch("/user")
+      .then((data) => data.json())
+      .then((data) => {
+        if (data === null) {
           setIsLoggedIn(false);
-          setUser({username:'', password:'', name:'', lastName:'', phoneNumber:'', email:'', role:'', confirmed:false});
-        }else{
+          setUser({
+            username: "",
+            password: "",
+            name: "",
+            lastName: "",
+            phoneNumber: "",
+            email: "",
+            role: "",
+            confirmed: false,
+          });
+        } else {
           setIsLoggedIn(true);
           setUser(data);
         }
-    })
+      });
   }
 
-  if(isLoggedIn !== true){
-    return(
+  if (isLoggedIn !== true) {
+    return (
       <BrowserRouter>
-        <HeaderLoggedOut/>
+        <HeaderLoggedOut />
         <div className="App">
           <Switch>
-            <Login path='/login' onLogin={checkUserStatus}/>
-            <Route path='/register' exact component={Register}/>
+            <Login path="/login" onLogin={checkUserStatus} />
+            <Route path="/register" exact component={Register} />
           </Switch>
         </div>
       </BrowserRouter>
@@ -45,31 +62,24 @@ function App() {
   }
 
   return (
-    <React.Fragment>
     <BrowserRouter>
-      <HeaderLoggedIn onLogout={checkUserStatus} currentUser={user.userName} role={user.role}/>
+      <HeaderLoggedIn
+        onLogout={checkUserStatus}
+        currentUser={user.userName}
+        role={user.role}
+      />
       <div className="App">
         <Switch>
-          <Route path='/' exact component={Test}/>
-          <Route path='/register' exact component={Register}/>
-          <Route path='/users' exact component={Users}/>
-          <Route path='/test' exact component={Test}/>
+          <Route path="/" exact component={Test} />
+          <Route path="/register" exact component={Register} />
+          <Route path="/users" exact component={Users} />
+          <Route path="/test" exact component={Test} />
         </Switch>
       </div>
+      <div>
+        <Map />
+      </div>
     </BrowserRouter>
-
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-  <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
-  <Marker position={[51.505, -0.09]}>
-    <Popup>
-      A pretty CSS3 popup. <br /> Easily customizable.
-    </Popup>
-  </Marker>
-</MapContainer>
-    </React.Fragment>
   );
 }
 
---
+++ IzvorniKod/frontend/src/Map.js
@@ -0,0 +1,20 @@
+import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+function Map() {
+  return (
+    <MapContainer center={[43.505, 16.09]} zoom={13} scrollWheelZoom={true}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[43.505, 16.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+}
+
+export default Map;
