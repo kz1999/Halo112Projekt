d5b1194f7d881e0af8285643f8d7f252c6c10296
Florijan Rusac <florijan.rusac@gmail.com>
2022-01-07 16:43:05 +0100

    hardkodirane akcije, lokacije, stanice

--- IzvorniKod/backend/src/main/resources/data.sql
+++ IzvorniKod/backend/src/main/resources/data.sql
@@ -3,3 +3,12 @@ INSERT INTO myuser(id, user_Name, photo, password_Hash, name, surname, phone_Num
 INSERT INTO myuser(id, user_Name, photo, password_Hash, name, surname, phone_Number, email, role, confirmed) VALUES (103, 'admin', '/user3photo', '$2a$12$RFv9B3TthXaQ/3MdjZFzxuigjmcP518d3mcvR9RKo9H4dufMWR0Iy', 'user3name', 'user3surname', '091', 'user3mail', 'admin', true);
 INSERT INTO myuser(id, user_Name, photo, password_Hash, name, surname, phone_Number, email, role, confirmed) VALUES (104, 'fireman', '/user4photo', '$2a$12$RFv9B3TthXaQ/3MdjZFzxuigjmcP518d3mcvR9RKo9H4dufMWR0Iy', 'user4name', 'user4surname', '091', 'user4mail', 'fireman', true);
 INSERT INTO myuser(id, user_Name, photo, password_Hash, name, surname, phone_Number, email, role, confirmed) VALUES (105, 'doctor', '/user5photo', '$2a$12$RFv9B3TthXaQ/3MdjZFzxuigjmcP518d3mcvR9RKo9H4dufMWR0Iy', 'user5name', 'user5surname', '091', 'user5mail', 'doctor', true);
+
+INSERT INTO location(id, name, x, y) VALUES (101, 'imeLokacije1', 1, 1);
+INSERT INTO location(id, name, x, y) VALUES (102, 'imeLokacije2', 2, 2);
+
+INSERT INTO action(id, description, urgency, location_id) VALUES (101, 'opis1', 1, 101);
+INSERT INTO action(id, description, urgency, location_id) VALUES (102, 'opis2', 2, 102);
+
+INSERT INTO station(id, director_id, location_id, name, type) VALUES (101, 101, 101, 'imeStanice1', 0);
+INSERT INTO station(id, director_id, location_id, name, type) VALUES (102, 104, 102, 'imeStanice2', 1);
