6b1a2ada097c83044d0b630f157c401470c89ad1
luka <luka.ivankovic2@fer.hr>
2022-01-08 00:21:52 +0100

    dodano za dodavanje ljudi na akcije

--- IzvorniKod/frontend/src/Actions.js
+++ IzvorniKod/frontend/src/Actions.js
@@ -3,11 +3,19 @@ import './styles/App.css';
 
 function Actions(){
     const [allActions, setAllActions] = React.useState([]);
-    
+    const [form, setForm] = React.useState( {member_id:null});
+    const [stations, setStations] = React.useState([]);
+
     React.useEffect(()=>{
         fetch('/akcije')
         .then(data => data.json())
         .then(allActions => setAllActions(allActions));
+    }, []);
+
+    React.useEffect(()=>{
+        fetch('/stanice')
+        .then(data => data.json())
+        .then(stations => setStations(stations));
     }, []);
 
     function onClick1(event){
@@ -28,6 +36,20 @@ function Actions(){
         fetch('/akcije/location/8', options).then(data => console.log(data));
     }
 
+    function onSubmit(event){
+        event.preventDefault();
+        const options={
+            method: 'POST',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body: form.member_id
+        };
+        console.log(form.member_id)
+        //postalji zahtjev tom memberu da je na toj akciji
+        //fetch('/stanice/members/'+form.station_id, options);
+    }
+
     function addAction(event){
         event.preventDefault();
         
@@ -45,6 +67,12 @@ function Actions(){
 
         fetch('/akcije', options).then(response=>response.json()).then(response=>console.log(response))
     }
+    
+    
+    function onChange(event){
+        const {name, value} = event.target;
+        setForm(oldForm => ({...oldForm, [name]: value}))
+    }
 
     return(
 
@@ -52,6 +80,24 @@ function Actions(){
             <h2>Actions</h2>
             <div><button onClick={addAction}>addAction</button></div>
             { allActions.map(akcija => <div key={akcija.id}>{akcija.description}</div>)}
+            <form onSubmit={onSubmit}>
+                
+                <div className="FormRow">
+                    <label>member</label>
+                    <select name='member_id' onChange={onChange} value={form.member_id}>
+                        <option value={null}>Odaberi</option>
+                        
+                        {stations.map(station => 
+                            <optgroup key={station.id} label={station.name}>
+                                {
+                                    station.members.map(member_id => <option key = {member_id} value={member_id}>{member_id}</option>)
+                                }
+                            </optgroup>)
+                        }
+                    </select>
+                </div>
+                <button type="submit">Add</button>
+            </form>
         </div>
     )
 }
--- IzvorniKod/frontend/src/Header.js
+++ IzvorniKod/frontend/src/Header.js
@@ -42,19 +42,17 @@ function HeaderLoggedIn(props){
     else if(props.role === "fireman" || props.role === "policeman" || props.role === "doctor"){
         //fetch('fireman') je sada, vraca sve firemane, slicno za 'police', za doktore nisam uspio otkriti.
         //fetch('fireman/id') bi vracao abilities, to mi netreba trenutacno
-        //fetch('responder') vraca director boolean -> otkriva mu se stranica za stanicu
+        //fetch('responder') vraca director boolean -> otkriva mu se stranica za stanicu i dodavanje ljudi u nju
 
         //ispod statusa bi pisao popis akcija na koje je on pozvan, moze se odazvati samo na jednu, onda mu se otkriva mapa i zatvara /status
+        //popis stanica bi bio u obliku pendingActions i currentAction (id)
         return(
             <header className="Header">
                 <header className="App-header-container">
                     <div className="App-header">
                         <Link to='/station'>Add member to your station</Link>
                         <Link to='/test'>Test</Link>
-                        <Link to='/status'>Status</Link> 
-                        
-                            
-                        
+                        <Link to='/status'>Status</Link>
                         <Link to='/map'>Map</Link>       
                         <button onClick={logout}> user: {props.currentUser}, role: {props.role}<div/>Logout!</button>
                     </div>
