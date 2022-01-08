05e342a19d515f59b564cc2b4c3949987100de7f
Krunoslav Zadrić <kz51900@fer.hr>
2022-01-08 13:26:49 +0100

    dinamicko dodavanje ikona

--- IzvorniKod/frontend/src/Map.js
+++ IzvorniKod/frontend/src/Map.js
@@ -1,23 +1,68 @@
-import React from "react";
-import './styles/App.css';
-import { MapContainer, TileLayer, useMapEvents } from "react-leaflet";
+import React, { useState } from "react";
+import "./styles/App.css";
+import L from "leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  useMapEvents,
+  Popup,
+  Marker,
+} from "react-leaflet";
 import RoutingMachine from "./Routing";
+import { LatLng } from "leaflet";
+import carMarker from "./images/car.png";
+import rescMarker from "./images/person.png";
 
+function Map() {
+  const iconCar = new L.Icon({
+    iconUrl: carMarker,
+    iconRetinaUrl: carMarker,
+    iconAnchor: null,
+    popupAnchor: null,
+    shadowUrl: null,
+    shadowSize: null,
+    shadowAnchor: null,
+    iconSize: new L.Point(40, 40),
+    className: "leaflet-div-icon",
+  });
 
-function Map(){
+  function LocationMarkers() {
+    const initialMarkers: LatLng[] = [new LatLng(46, 16)];
+    const [markers, setMarkers] = useState(initialMarkers);
 
-    return(
-        <div className="Map">
-            <h2>Map</h2>
-            <MapContainer center={[45.8, 16]} zoom={13} scrollWheelZoom={true} closePopupOnClick={true}>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                 />
-                <RoutingMachine />
-            </MapContainer>
-        </div>
-    )
+    const map = useMapEvents({
+      click(e) {
+        markers.push(e.latlng);
+        setMarkers((prevValue) => [...prevValue, e.latlng]);
+      },
+    });
+    return (
+      <React.Fragment>
+        {markers.map((marker) => (
+          <Marker position={marker} icon={iconCar}></Marker>
+        ))}
+      </React.Fragment>
+    );
+  }
+
+  return (
+    <div className="Map">
+      <h2>Map</h2>
+      <MapContainer
+        center={[45.8, 16]}
+        zoom={13}
+        scrollWheelZoom={true}
+        closePopupOnClick={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <RoutingMachine />
+        <LocationMarkers />
+      </MapContainer>
+    </div>
+  );
 }
 
 export default Map;
