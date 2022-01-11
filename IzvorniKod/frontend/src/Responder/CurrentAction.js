57f1641870cb2e13672660c370b1c684bdd02361
Krunoslav Zadrić <kz51900@fer.hr>
2022-01-11 15:09:30 +0100

    dohvacanje lokacije usera

--- IzvorniKod/frontend/src/Responder/CurrentAction.js
+++ IzvorniKod/frontend/src/Responder/CurrentAction.js
@@ -2,7 +2,13 @@ import React, { Component, useState } from "react";
 import "../styles/App.css";
 import "../styles/Switch.css";
 
-import {MapContainer,TileLayer,useMapEvents,Popup,Marker,} from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  useMapEvents,
+  Popup,
+  Marker,
+} from "react-leaflet";
 import * as L from "leaflet";
 import { LatLng } from "leaflet";
 import { createControlComponent } from "@react-leaflet/core";
@@ -16,94 +22,135 @@ import carMarker from "../images/car.png";
 import rescMarker from "../images/person.png";
 
 function CurrentAction(props) {
-    const [responderLocation, setResponderLocation] = React.useState(new LatLng(0, 0));
-    const [respondersLocations, setRespondersLocations] = React.useState([]);
-    const [taskLocations, setTaskLocations] = React.useState({});
+  const [responderLocation, setResponderLocation] = React.useState(
+    new LatLng(0, 0)
+  );
+  const [respondersLocations, setRespondersLocations] = React.useState([]);
+  const [taskLocations, setTaskLocations] = React.useState({});
 
-    const iconPerson = new L.Icon({
-        iconUrl: rescMarker,
-        iconRetinaUrl: rescMarker,
-        iconAnchor: null,
-        popupAnchor: null,
-        shadowUrl: null,
-        shadowSize: null,
-        shadowAnchor: null,
-        iconSize: new L.Point(40, 40),
-        //className: "leaflet-div-icon",
+  const iconPerson = new L.Icon({
+    iconUrl: rescMarker,
+    iconRetinaUrl: rescMarker,
+    iconAnchor: null,
+    popupAnchor: null,
+    shadowUrl: null,
+    shadowSize: null,
+    shadowAnchor: null,
+    iconSize: new L.Point(40, 40),
+    //className: "leaflet-div-icon",
+  });
+
+  React.useEffect(() => {
+    fetch("/spasioci/current")
+      .then((spasioc) => spasioc.json())
+      .then((spasioc) => {
+        if (spasioc !== undefined) {
+          fetch("/lokacija/" + spasioc.location_id)
+            .then((location) => location.json())
+            .then((location) => {
+              setResponderLocation(new LatLng(location.x, location.y));
+            });
+          fetch("/akcije/" + spasioc.currentAction_id)
+            .then((akcija) => akcija.json())
+            .then((akcija) => akcija.tasks)
+            .then((tasks) =>
+              tasks.filter((task) => task.responder_id === spasioc.id)
+            )
+            .then((tasks) => {
+              tasks.map((task) => {
+                task.location_id.map((location_id) => {
+                  fetch("/lokacija/" + location_id)
+                    .then((location) => location.json())
+                    .then((location) => {
+                      if (task.id in taskLocations) {
+                        taskLocations[task.id].push(
+                          new LatLng(location.x, location.y)
+                        );
+                      } else {
+                        taskLocations[task.id] = [
+                          new LatLng(location.x, location.y),
+                        ];
+                      }
+                    });
+                });
+              });
+            });
+          fetch("/spasioci")
+            .then((spasioci) => spasioci.json())
+            .then((spasioci) =>
+              spasioci.filter(
+                (kolega) =>
+                  kolega.currentAction_id === spasioc.currentAction_id &&
+                  kolega.id !== spasioc.id
+              )
+            )
+            .then((spasioci) =>
+              spasioci.map((kolega) => {
+                fetch("/lokacija/" + kolega.currentAction_id)
+                  .then((location) => location.json())
+                  .then((location) =>
+                    respondersLocations.push(new LatLng(location.x, location.y))
+                  );
+              })
+            );
+        }
+      });
+  }, []);
+
+  const createRoutineMachineLayer = (props) => {
+    const instance = L.Routing.control({
+      waypoints: props.waypoints,
+      lineOptions: {
+        styles: [{ color: "#6FA1EC", weight: 4 }],
+      },
+      addWaypoints: true,
+      fitSelectedRoutes: true,
+      showAlternatives: false,
+      //geocoder: L.Control.Geocoder.nominatim(),
+    });
+    return instance;
+  };
+  const RoutingMachine = createControlComponent(createRoutineMachineLayer);
+
+  function LocateUser() {
+    const map = useMapEvents({
+      keypress() {
+        map.locate();
+      },
+      locationfound(e) {
+        setResponderLocation(e.latlng);
+      },
     });
 
-    React.useEffect(() => {
-        fetch("/spasioci/current")
-        .then(spasioc => spasioc.json())
-        .then(spasioc => {
-            if(spasioc !== undefined){
-            fetch("/lokacija/"+spasioc.location_id)
-            .then(location => location.json())
-            .then(location => {setResponderLocation(new LatLng(location.x, location.y))})
-            fetch("/akcije/"+spasioc.currentAction_id)
-            .then(akcija => akcija.json())
-            .then(akcija => akcija.tasks)
-            .then(tasks => tasks.filter(task => task.responder_id === spasioc.id))
-            .then(tasks => {
-                tasks.map(task => {
-                    task.location_id.map(location_id => {
-                        fetch("/lokacija/" + location_id)
-                        .then(location => location.json())
-                        .then(location => {
-                            if(task.id in taskLocations){
-                                taskLocations[task.id].push(new LatLng(location.x, location.y))
-                            }else{
-                                taskLocations[task.id] = [new LatLng(location.x, location.y)]}
-                    })})
-            })})
-            fetch("/spasioci")
-            .then(spasioci => spasioci.json())
-            .then(spasioci => spasioci.filter(kolega => kolega.currentAction_id === spasioc.currentAction_id && kolega.id !== spasioc.id))
-            .then(spasioci => spasioci.map(kolega => {
-                fetch("/lokacija/"+kolega.currentAction_id)
-                .then(location => location.json())
-                .then(location => respondersLocations.push(new LatLng(location.x, location.y)))
-            }))
-        }})
-    }, [])
+    return null;
+  }
 
-    const createRoutineMachineLayer = (props) => {
-        const instance = L.Routing.control({
-        waypoints: props.waypoints,
-        lineOptions: {
-            styles: [{ color: "#6FA1EC", weight: 4 }],
-        },
-        addWaypoints: true,
-        fitSelectedRoutes: true,
-        showAlternatives: false,
-        //geocoder: L.Control.Geocoder.nominatim(),
-        });
-        return instance;
-    };
-    const RoutingMachine = createControlComponent(createRoutineMachineLayer);
-
-    
-    return (
-        
-        <div className="Action map">
-            <h2>Action id: {props.currentAction_id}</h2>
-            <MapContainer
-                center={[45.8, 16]}
-                zoom={13}
-                scrollWheelZoom={true}
-                closePopupOnClick={true}>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
-                <React.Fragment>
-                <Marker position={responderLocation} icon={iconPerson}></Marker>
-                {respondersLocations.map(marker => <Marker key={marker} position={marker} ></Marker>)}
-                </React.Fragment>
-                {Object.values(taskLocations).map(task => <RoutingMachine key={task} waypoints={task}/>)}
-            </MapContainer>
-        </div>
-        
-    );
+  return (
+    <div className="Action map">
+      <h2>Action id: {props.currentAction_id}</h2>
+      <MapContainer
+        center={[45.8, 16]}
+        zoom={13}
+        scrollWheelZoom={true}
+        closePopupOnClick={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <React.Fragment>
+          <LocateUser />
+          <Marker position={responderLocation} icon={iconPerson}></Marker>
+          {respondersLocations.map((marker) => (
+            <Marker key={marker} position={marker}></Marker>
+          ))}
+        </React.Fragment>
+        {Object.values(taskLocations).map((task) => (
+          <RoutingMachine key={task} waypoints={task} />
+        ))}
+      </MapContainer>
+    </div>
+  );
 }
 
 export default CurrentAction;
