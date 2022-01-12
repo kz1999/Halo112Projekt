d9d8e83c32f87e39f6c3ca8fa0138dba0d6e83c2
Krunoslav Zadrić <kz51900@fer.hr>
2022-01-12 14:12:07 +0100

    popravak buga u responder action manageru

--- IzvorniKod/frontend/src/Responder/ResponderActionMenager.js
+++ IzvorniKod/frontend/src/Responder/ResponderActionMenager.js
@@ -3,42 +3,40 @@ import CurrentAction from "./CurrentAction";
 import Status from "./Status";
 import Request from "./Request";
 
-function ResponderActionMenager(){
-    const [currentAction_id, setCurrentAction_id] = React.useState(null);
-    const [requestsList, setRequestsList] = React.useState([]);
+function ResponderActionMenager() {
+  const [currentAction_id, setCurrentAction_id] = React.useState(null);
+  const [requestsList, setRequestsList] = React.useState([]);
 
-    React.useEffect(()=>{
-        fetch('/spasioci/current')
-        .then(response => {
-            if(response.status === 200)
-                return response.json();
-            return undefined;
-        })
-        .then(data => {
-            if(data !== undefined){
-                setRequestsList(data.requestsList)
-                setCurrentAction_id(data.currentAction_id)
-            }
-        });
-    });
+  React.useEffect(() => {
+    fetch("/spasioci/current")
+      .then((response) => {
+        if (response.status === 200) return response.json();
+        return undefined;
+      })
+      .then((data) => {
+        if (data !== undefined) {
+          setRequestsList(data.requestsList);
+          setCurrentAction_id(data.currentAction_id);
+        }
+      });
+  }, []);
 
-    if(currentAction_id !== null){
-        return(
-            <div className="ActionMenager">
-                <CurrentAction currentAction_id={currentAction_id}/>
-            </div>
-        )
-    }
-    return(
-        <div className="ActionMenager">
-            <Status/>
-            <h2>Pozivi na akciju:</h2>
-            {
-                requestsList.map(request_id=> 
-                <Request key={request_id}request_id={request_id}></Request>)
-            }
-        </div>
-    )
+  if (currentAction_id !== null) {
+    return (
+      <div className="ActionMenager">
+        <CurrentAction currentAction_id={currentAction_id} />
+      </div>
+    );
+  }
+  return (
+    <div className="ActionMenager">
+      <Status />
+      <h2>Pozivi na akciju:</h2>
+      {requestsList.map((request_id) => (
+        <Request key={request_id} request_id={request_id}></Request>
+      ))}
+    </div>
+  );
 }
 
 export default ResponderActionMenager;
