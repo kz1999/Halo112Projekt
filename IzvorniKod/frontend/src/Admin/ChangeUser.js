765059d9aa9c3564508b060e5294c26580b741c6
Lukas Ujcic <lu52326@fer.hr>
2022-01-14 13:48:58 +0100

    css komentari i change user

--- IzvorniKod/frontend/src/Admin/ChangeUser.js
+++ IzvorniKod/frontend/src/Admin/ChangeUser.js
@@ -1,5 +1,6 @@
 import React from "react";
 import '../styles/App.css';
+import '../styles/ChangeUser.css'
 
 function ChangeUser(props){
     
@@ -44,27 +45,27 @@ function ChangeUser(props){
             <h2>Change user: {form.userName}</h2>
             <form onSubmit={onSubmit}>
                 <div className="FormRow">
-                    <label>Password</label>
-                    <input name='password' onChange={onChange} value={form.passwordHash}/>
+                    <label className="form-label">Password:</label>
+                    <input className="form-field" name='password' onChange={onChange} value={form.passwordHash}/>
                 </div>
                 <div className="FormRow">
-                    <label>E-mail adress</label>
-                    <input name='email' onChange={onChange} value={form.email}/>
+                    <label className="form-label">E-mail adress</label>
+                    <input className="form-field" name='email' onChange={onChange} value={form.email}/>
                 </div>
                 <div className="FormRow">
-                    <label>Name</label>
-                    <input name='name' onChange={onChange} value={form.name}/>
+                    <label className="form-label">Name</label>
+                    <input className="form-field" name='name' onChange={onChange} value={form.name}/>
                 </div>
                 <div className="FormRow">
-                    <label>Last name</label>
-                    <input name='surname' onChange={onChange} value={form.surname}/>
+                    <label className="form-label">Last name</label>
+                    <input className="form-field" name='surname' onChange={onChange} value={form.surname}/>
                 </div>
                 <div className="FormRow">
-                    <label>Telephone number</label>
-                    <input name='phoneNumber' onChange={onChange} value={form.phoneNumber}/>
+                    <label className="form-label">Telephone number</label>
+                    <input className="form-field" name='phoneNumber' onChange={onChange} value={form.phoneNumber}/>
                 </div>
                 <div className="FormRow">
-                    <label>Role</label>
+                    <label className="form-label">Role</label>
                     <select name ="role" onChange={onChange} value={form.role}>
                         <option value="dispatcher">Dispatcher</option>
                         <option value="fireman">Fireman</option>
@@ -76,7 +77,7 @@ function ChangeUser(props){
                 <div className ="FormRow">
                 <img name ="photo" src={form.photo} width="180" ></img>
                 </div>
-                <button type="submit">ChangeUser</button>
+                <button className="submit-button" type="submit">ChangeUser</button>
             </form>
         </div>
     )
